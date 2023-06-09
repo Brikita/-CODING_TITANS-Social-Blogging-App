36968302cdaf4fe26941fe2827636ee4e65bd678
Willy Laisa <122884320+Willylaisa@users.noreply.github.com>
2023-06-09 12:10:41 +0300

    Fixed javascript to make login and sign-up better

--- login.js
+++ login.js
@@ -1,29 +1,33 @@
+const userName = document.getElementById("name")
 const logIn = document.getElementById("login")
-const signUp = document.getElementById("signup")
 const eMail = document.getElementById("email")
 const passWord = document.getElementById("password")
 const errorMessage = document.getElementById("error-message")
+const signUp = document.getElementById("sign-up")
 
+function openIndex() {
+    let loginDetails ={
+        email: eMail.value,
+        password: passWord.value
+    }
+    localStorage.setItem("logindetails", JSON.stringify(loginDetails))
+    window.open("index.html")   
+}
 
 logIn.addEventListener("click", function() {
-    if (eMail.value === "titans@coding.com" && passWord.value === "Test@123") {
-        window.open("index.html")
+    const logIn = JSON.parse(localStorage.getItem("logindetails"))
+    if (eMail.value === logIn.email && passWord.value === logIn.password) {
+        window.open("home.html")
         eMail.value = ""
         passWord.value = ""
         errorMessage.textContent = ""
     } else {
-        errorMessage.textContent = "Wrong username or password. Please sign up."
+        errorMessage.textContent = "Wrong email or password. Please sign up."
     }
 })()
 
-signUp.addEventListener("click", function() {
-    if (eMail.value === "titans@coding.com" && passWord.value === "Test@123") {
-        window.open("index.html")
-        eMail.value = ""
-        passWord.value = ""
-        errorMessage.textContent = ""
-    } else {
-        errorMessage.textContent = "Wrong username or password. Please sign up."
-    }
-})()
+
+function openSignup(){
+    window.open("signup.html")
+}
 
