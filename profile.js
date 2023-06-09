3446a2a9cdad204a930b9fb9b04b4593131e2309
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-09 14:19:42 +0300

    Linking JavaScript file to the profile file

---
+++ profile.js
@@ -0,0 +1,14 @@
+
+
+console.log(userDetails)
+
+// username on profile page ****
+const userName = document.getElementById("user-name")
+userName.textContent = username.value
+
+// name and profile pic
+const savedName = document.getElementById("saved-name")
+const yourName = document.getElementById("your-name")
+
+
+
