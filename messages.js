419a1f916e56850e78db58a598a10dca7f64c87b
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-15 08:22:07 +0300

    creating div elements based on number of messages

--- messages.js
+++ messages.js
@@ -11,32 +11,55 @@ let messages = [
     } , 
     {
         userName: 'user3',
-        message: "Are you coming today"
+        message: "Are you coming today?"
     } , 
     {
         userName: 'user4',
-        message: "Is it today or tommorow"}
+        message: "Is it today or tommorow?"}
 ]
 
-
-const users = document.querySelectorAll('.user');
-const messageDisplay = document.querySelectorAll('.unread-messages');
+const unreadMessages = document.getElementById('unread');
+//const users = document.querySelectorAll('.user');
+//const messageDisplay = document.querySelectorAll('.unread-messages');
 const searchBtn = document.getElementById('search-btn');
 let searchBar = document.getElementById('search-bar');
 const searchResult = document.getElementById('search-msg');
+
+// Creating elements based on the number of messages
+for (let i = 0; i < messages.length ; i++ ){
+    const users = messages[i];
+
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('unread');
+
+    const name = document.createElement('div');
+    name.textContent = users.userName;
+    name.classList.add('user');
+
+    const messageDisplay = document.createElement('div');
+    messageDisplay.textContent = users.message;
+    messageDisplay.classList.add('unread-messages');
+
+    userDiv.appendChild(name);
+    userDiv.appendChild(messageDisplay)
+
+    unreadMessages.appendChild(userDiv)
+}
+
 
 
 //Displaying the users
 
 
-for (let i = 0 ; i < users.length ; i++ ) {
+/* for (let i = 0 ; i < users.length ; i++ ) {
     users[ i ].textContent = messages[i].userName;    
 }
 
 // display the messages
 for (let i = 0 ; i < messageDisplay.length ; i++ ){
     messageDisplay[i].textContent = messages[i].message;
-}
+} */
+
 
 //Search Bar
 searchBtn.addEventListener('click', function(){
@@ -45,11 +68,16 @@ searchBtn.addEventListener('click', function(){
     for (let i = 0 ; i < messages.length ; i++){
         if ( messages[i].userName === searchMsg ){
             result = messages[i].userName+": " +messages[i].message;
+        }else{
+            result = "Friend not found"
         }
 
     }
     searchResult.textContent = result;
     console.log(result)
+    searchResult.classList.remove('hidden')
     
 });
+//search Result
 
+searchResult.classList.add('hidden');
